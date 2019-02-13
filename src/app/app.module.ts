60a145f42cf8cfce1ad49cbee1ca109f44df0282
Sai Prakash Holi <holisaiprakash@gmail.com>
2019-02-14 02:34:50 +0530

    my first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { IndexComponent } from './CustomComponents/index/index.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    IndexComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
